1b1974e4bc2264a6b20b5b3685e4bf528257b311
darambae <64197421+jazzy1101@users.noreply.github.com>
2023-11-24 19:58:07 +0100

    capitalizer ongloing

--- level-3/print_hex/print_hex.c
+++ level-3/print_hex/print_hex.c
@@ -5,24 +5,28 @@ int	ft_atoi(char *s)
 {
 	int	res = 0;
 	while (*s)
+	{
 		res = res * 10 + *s - '0';
+		s++;
+	}
 	return res;	
 }
+void	print_hex(int decimal)
+{
+	char    hex[] = "0123456789abcdef";
 
+	if (decimal >= 16)
+		print_hex(decimal / 16);
+	write(1, &hex[decimal % 16], 1);
+}
 int	main(int argc, char **argv)
 {
 	int	decimal;
-	char	hex = "0123456789abcdef";
-	
+
 	if (argc == 2)
 	{
 		decimal = ft_atoi(argv[1]);
-		printf("%d\n", decimal);
-		while (decimal > 0)
-		{
-			decimal /= 16;
-			write(1, &hex[decimal % 16], 1);
-		}	
+		print_hex(decimal);
 	}
 	write(1, "\n", 1);
 	return (0);
